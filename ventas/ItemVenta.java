c822a5884a36b4ba8973a71429379564eb026059
svidler17 <charlytorresj1708@gmail.com>
2025-11-28 12:24:11 -0500

    creacion de clase item

---
+++ ventas/ItemVenta.java
@@ -0,0 +1,29 @@
+package ventas;
+
+public class ItemVenta {
+
+    private String codigo;
+    private String nombre;
+    private int cantidad;
+    private double subtotal;
+
+    public ItemVenta(String codigo, String nombre, int cantidad, double subtotal) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.subtotal = subtotal;
+    }
+
+    public String getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+    public int getCantidad() { return cantidad; }
+    public double getSubtotal() { return subtotal; }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "  - %s (%s) x%d  →  Subtotal: $%.2f",
+                nombre, codigo, cantidad, subtotal
+        );
+    }
+}
